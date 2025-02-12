520857e68fe037ea857ede9180c89bb740cbc86d
AjChamPy <alicham2005@gmail.com>
2025-02-11 21:14:17 -0500

    Adicion del primer punto del taller

--- App.java
+++ App.java
@@ -1,6 +1,11 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Metodos mainMetodos  = new Metodos();
+
+        Scanner sc = new Scanner(System.in);
 
         int[] dimensiones = Metodos.preguntarDimensionMatriz();
         int filas = dimensiones[0];
@@ -8,6 +13,16 @@ public class App {
 
         int[][] matriz = mainMetodos.llenarMatriz(filas, columnas);
         mainMetodos.mostrarMatriz(matriz);
+        System.out.println("Ingrese el ejercicio el cual quiera visualizar (del 1 al 11): ");
+        int opt = sc.nextInt();
+
+        switch (opt) {
+            case 1 -> {
+                Puntos punto1 = new Puntos();
+                punto1.sumarValores(matriz);
+            }
+            default -> throw new AssertionError();
+        }
         
 
     }
--- Puntos.java
+++ Puntos.java
@@ -1,3 +1,13 @@
 public class Puntos {
-    
+    //Punto_1
+    public int sumarValores(int[][] matriz){
+        int suma = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                suma = suma + matriz[i][j];
+            }
+        }
+        System.out.println("La suma de lo valores de la matriz es de: "+ suma);
+        return suma;
+    }
 }
